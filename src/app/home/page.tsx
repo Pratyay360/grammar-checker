ead33c1754fb1bcbf617b0d9bc5b73ad94cd1326
Pratyay Mustafi <pratyaymustafi@gmail.com>
2024-07-02 20:38:25 +0530

    added summarizer and custom 404 page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -54,6 +54,51 @@ export default function Grammar() {
             toast.success("Rewritten successfully")
         }
     }
+    async function summarizer() {
+        // console.log(API)
+        const genAI = new GoogleGenerativeAI(API);
+        const MODEL_NAME = "gemini-pro";
+        const generationConfig = {
+            temperature: 0.45,
+            candidateCount: 1,
+            topK: 1,
+            topP: 1,
+            maxOutputTokens: 1024,
+        };
+        const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+        const safetySettings = [
+            {
+                category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+                threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            },
+        ];
+        let input = document.getElementById("input") as HTMLInputElement
+        let text = input.value || ''
+        if (text.length == 0) {
+            toast.error("Please enter a sentence to check")
+        } else {
+            sessionStorage.setItem("oldSentence", text)
+            const parts = [
+                { text: "summarize the entire text corretly (GIVE ONLY 1 RESPONSE) : " + text },
+            ];
+
+            const result = await model.generateContent({
+                contents: [{ role: "user", parts }],
+                generationConfig,
+                safetySettings,
+            });
+            // candidates[0].content.parts[0].text
+
+            const response = result.response;
+            let a = Array.isArray(response) ? response[0]?.candidates?.[0]?.content?.parts?.[0]?.text : response?.candidates?.[0]?.content?.parts?.[0]?.text
+            a=a.replace(/"/g, '')
+            a=a.replace(/,"/g, '')
+            a=a.replace(/."/g, '')
+            a=a.split('"').join('')
+            input.value = a 
+            toast.success("Summarized successfully")
+        }
+    }
     async function grammarCheck() {
         // console.log(API)
         const genAI = new GoogleGenerativeAI(API);
@@ -133,7 +178,8 @@ export default function Grammar() {
             </div>
             <div className="flex flex-wrap items-center justify-center center gap-5">
                 <button onClick={grammarCheck} id="correction" className="border border-indigo-600 hover:bg-indigo-600 bg-black text-white rounded-lg py-3 font-semibold px-2">Grammar Correction</button>
-                <button onClick={rewriteSentence} id="rewrite" className="border border-indigo-600 hover:bg-indigo-600 bg-black text-white rounded-lg py-3 font-semibold px-2">Rewrite Sentence</button>
+                <button onClick={rewriteSentence} id="rewrite" className="border border-indigo-600 hover:bg-indigo-600 bg-black text-white rounded-lg py-3 font-semibold px-2">Rephrase Sentence</button>
+                <button onClick={summarizer} id="correction" className="border border-indigo-600 hover:bg-indigo-600 bg-black text-white rounded-lg py-3 font-semibold px-2">Summarize Text</button>
                 <button onClick={replacePrevious} id="replace" className="border border-indigo-600 hover:bg-indigo-600 bg-black text-white rounded-lg py-3 font-semibold px-2">Replace Previous</button>
                 <button onClick={copy} id="cpy" className="border border-indigo-600 hover:bg-indigo-600 bg-black text-white rounded-lg py-3 font-semibold px-2">Copy</button>
             </div>
