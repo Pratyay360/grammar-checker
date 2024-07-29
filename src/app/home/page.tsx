56c4d2a7f3407d088dc167275e3208e0f4ee5aba
Pratyay Mustafi <pratyaymustafi@gmail.com>
2024-07-29 12:08:42 +0530

    Refactor code to improve readability and maintainability

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,95 +2,94 @@
 import React from "react";
 import { Toaster, toast } from "sonner";
 
+async function rewriteSentence() {
+  let input = document.getElementById("input") as HTMLInputElement;
+  let text = input.value || "";
+  if (text.length == 0) {
+    toast.error("Please enter a sentence to rewrite");
+  } else {
+    sessionStorage.setItem("oldSentence", text);
+    const response = await fetch("/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(
+        "rewrite this sentence like a pro (GIVE ONLY 1 RESPONSE) : " + text
+      ),
+    });
+    input.value = await response.text();
+    toast.success("rewritting successfully");
+  }
+}
+async function summarizer() {
+  let input = document.getElementById("input") as HTMLInputElement;
+  let text = input.value || "";
+  if (text.length == 0) {
+    toast.error("Please enter a sentence to summarize");
+  } else {
+    sessionStorage.setItem("oldSentence", text);
+    const response = await fetch("/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(
+        "summarize this sentence like a pro (GIVE ONLY 1 RESPONSE) : " + text
+      ),
+    });
+    input.value = await response.text();
+    toast.success("Summarized successfully");
+  }
+}
+async function grammarCheck() {
+  // console.log(API)
+  let input = document.getElementById("input") as HTMLInputElement;
+  let text = input.value || "";
+  if (text.length == 0) {
+    toast.error("Please enter a sentence to summarize");
+  } else {
+    sessionStorage.setItem("oldSentence", text);
+    const response = await fetch("/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(
+        "make this sentence grammatically correct like a pro (GIVE ONLY 1 RESPONSE) : " +
+          text
+      ),
+    });
+    input.value = await response.text();
+    toast.success("Corrected successfully");
+  }
+}
+
+async function replacePrevious() {
+  let input = document.getElementById("input") as HTMLInputElement;
+  let old = sessionStorage.getItem("oldSentence");
+  if ((old?.length ?? 0) > 0) {
+    // console.log(old)
+    input.value = old as string;
+    toast.success("Replaced successfully");
+  } else {
+    toast.error("No previous sentence found");
+  }
+}
+function copy() {
+  let input = document.getElementById("input") as HTMLInputElement;
+  // input.select()
+  let a = input.value;
+  if (a.length > 0) {
+    navigator.clipboard.writeText(a).then(async function () {
+      toast.success("Copied successfully");
+    });
+  } else {
+    toast.error("No text to copy");
+  }
+}
+
 export default function Grammar() {
-  async function rewriteSentence() {
-    let input = document.getElementById("input") as HTMLInputElement;
-    let text = input.value || "";
-    if (text.length == 0) {
-      toast.error("Please enter a sentence to rewrite");
-    } else {
-      sessionStorage.setItem("oldSentence", text);
-      const response = await fetch("/api", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(
-          "rewrite this sentence like a pro (GIVE ONLY 1 RESPONSE) : " +
-            text
-        ),
-      });
-      input.value = await response.text();
-      toast.success("rewritting successfully");
-    }
-  }
-  async function summarizer() {
-    let input = document.getElementById("input") as HTMLInputElement;
-    let text = input.value || "";
-    if (text.length == 0) {
-      toast.error("Please enter a sentence to summarize");
-    } else {
-      sessionStorage.setItem("oldSentence", text);
-      const response = await fetch("/api", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(
-          "summarize this sentence like a pro (GIVE ONLY 1 RESPONSE) : " + text
-        ),
-      });
-      input.value = await response.text();
-      toast.success("Summarized successfully");
-    }
-  }
-  async function grammarCheck() {
-    // console.log(API)
-    let input = document.getElementById("input") as HTMLInputElement;
-    let text = input.value || "";
-    if (text.length == 0) {
-      toast.error("Please enter a sentence to summarize");
-    } else {
-      sessionStorage.setItem("oldSentence", text);
-      const response = await fetch("/api", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(
-          "make this sentence grammatically correct like a pro (GIVE ONLY 1 RESPONSE) : " +
-            text
-        ),
-      });
-      input.value = await response.text();
-      toast.success("Corrected successfully");
-    }
-  }
-
-  async function replacePrevious() {
-    let input = document.getElementById("input") as HTMLInputElement;
-    let old = sessionStorage.getItem("oldSentence");
-    if ((old?.length ?? 0) > 0) {
-      // console.log(old)
-      input.value = old as string;
-      toast.success("Replaced successfully");
-    } else {
-      toast.error("No previous sentence found");
-    }
-  }
-  function copy() {
-    let input = document.getElementById("input") as HTMLInputElement;
-    // input.select()
-    let a = input.value;
-    if (a.length > 0) {
-      navigator.clipboard.writeText(a).then(async function () {
-        toast.success("Copied successfully");
-      });
-    } else {
-      toast.error("No text to copy");
-    }
-  }
-
   return (
     <>
       <div className="flex flex-wrap items-center justify-center center mt-10 m-20 p-2">
